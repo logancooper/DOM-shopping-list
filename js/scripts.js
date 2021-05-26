beec15396f99caf48572fe6c4442914b62168877
Logan Cooper <logancooper@bellsouth.net>
2021-05-26 15:40:09 -0400

    Solved the exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
 'use strict';
+const submitButton = document.querySelector("#generateList");
+const groceryList = document.querySelector("#myList");
+let inputs = document.querySelectorAll(".input")
+
+
+submitButton.addEventListener("click", function()
+{
+    let inputMap = Array.prototype.map.call(inputs, function(x)
+    {
+        return x.value
+    })
+
+    inputMap.forEach(element => {
+        const newListElement = document.createElement("li");
+        newListElement.innerText = element;
+        groceryList.append(newListElement);
+    });
+
+})
